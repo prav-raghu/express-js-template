b6c6e39e8630d83985e2f35dd9780b0b2a9872e4
Prav <pravir.raghu@hotmail.com>
2022-01-13 10:36:42 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,5 @@ const app = express();
 const port = 3000;
 app.use("/", router)
 app.listen(port, () => {
-    console.log(`Application running on ${port}.`);
+    console.log(`Application running on http://localhost:${port}.`);
 });
