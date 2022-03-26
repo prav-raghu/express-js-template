5263ef294a0808b917ec567c708a8b79ff80d2f1
Prav <pravir.raghu@hotmail.com>
2022-03-26 13:45:36 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import express, { Application } from "express";
+import envalid = require("envalid");
 import { router } from "./routes/base.route";
 import { Swagger } from "./common/swagger";
-import envalid = require("envalid");
 const path = require("path");
+const pckg = require("../package.json");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const specs = swaggerJsdoc(Swagger.options);
 const { num, bool, str } = envalid;
 const env = envalid.cleanEnv(process.env, {
     PORT: num({ default: 3000 }),
@@ -10,16 +14,29 @@ const env = envalid.cleanEnv(process.env, {
     HOST: str({ default: "localhost:3000" }),
     NODE_ENV: str({ default: "development" }),
 });
-const pckg = require("../package.json");
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-const specs = swaggerJsdoc(Swagger.options);
 const port = env.PORT;
-const app: Application = express();
-app.use("/documentation", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
-app.use("/", router);
-app.use("/", express.static(path.join(__dirname, "static")));
-app.listen(port, () => {
-    console.log(`😎 express-js-template@${pckg.version} online ${env.NODE_ENV == "development" ? `http://localhost:${env.PORT}` : "express-js-template.co.za"} in ${env.NODE_ENV} mode.😎`);
-    console.log(`📖 documentation available here: ${env.NODE_ENV == "development" ? `http://localhost:${env.PORT}` : "mytelnet-auth-service.co.za"}/documentation 📖`);
-});
+
+export class Main {
+    private app: Application;
+
+    constructor() {
+        this.app = express();
+        this.app.use("/documentation", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
+        this.app.use("/", router);
+        this.app.use("/", express.static(path.join(__dirname, "static")));
+    }
+
+    public start = () => {
+        return new Promise((resolve, reject) => {
+            this.app
+                .listen(port, () => {
+                    console.log(`😎 express-js-template@${pckg.version} online ${env.NODE_ENV == "development" ? `http://localhost:${env.PORT}` : "express-js-template.co.za"} in ${env.NODE_ENV} mode.😎`);
+                    console.log(`📖 documentation available here: ${env.NODE_ENV == "development" ? `http://localhost:${env.PORT}` : "mytelnet-auth-service.co.za"}/documentation 📖`);
+                    resolve(port);
+                })
+                .on("error", (err: object) => reject(err));
+        });
+    };
+}
+
+new Main().start();
